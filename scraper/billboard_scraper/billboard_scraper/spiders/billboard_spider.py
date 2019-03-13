790ec5a600d9015a3b78bdfe7836f1b1f8b10d1f
Kevin Zhang <izkevin52@gmail.com>
2019-03-12 22:30:57 -0400

    updated requirements.txt file

--- scraper/billboard_scraper/billboard_scraper/spiders/billboard_spider.py
+++ scraper/billboard_scraper/billboard_scraper/spiders/billboard_spider.py
@@ -10,7 +10,7 @@ class QuotesSpider(scrapy.Spider):
             'country': 'https://www.billboard.com/charts/year-end/2018/hot-country-songs',
             'pop': 'https://www.billboard.com/charts/year-end/2018/pop-songs',
             'hiphop': 'https://www.billboard.com/charts/year-end/2018/hot-r-and-and-b-hip-hop-songs',
-            'latin': 'https://www.billboard.com/charts/year-end/2018/top-latin-artists',
+            'latin': 'https://www.billboard.com/charts/year-end/2018/hot-latin-songs',
             'electronic': 'https://www.billboard.com/charts/year-end/2018/hot-dance-electronic--songs',
             'christian': 'https://www.billboard.com/charts/year-end/2018/hot-christian-songs',
             'rock': 'https://www.billboard.com/charts/year-end/2018/hot-rock-songs'
@@ -21,9 +21,13 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
       genre = response.meta['genre']
       for song in response.css('div.ye-chart-item__text'):
+        if song.css('div.ye-chart-item__artist a').get():
+          artist = song.css('div.ye-chart-item__artist a::text').get()
+        else:
+          artist = song.css('div.ye-chart-item__artist::text').get()
         yield {
-            'song': song.css('div.ye-chart-item__title::text').get(),
-            'artist': song.css('div.ye-chart-item__artist::text').get(),
+            'song': song.css('div.ye-chart-item__title::text').get()[1:-1],
+            'artist': artist[1:-1],
             'genre': genre
         }
       next_pages = response.css('a.year-link::attr(href)').getall()
