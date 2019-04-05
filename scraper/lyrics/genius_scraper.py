32e371c4534ccc81b58907215e394599a20647fb
Kevin Zhang <izkevin52@gmail.com>
2019-04-05 11:57:03 -0400

    Added threading to the genius scraper

--- scraper/lyrics/genius_scraper.py
+++ scraper/lyrics/genius_scraper.py
@@ -2,28 +2,62 @@ import lyricsgenius
 import os
 from pathlib import Path
 import json
+import threading
 from dotenv import load_dotenv
+
+
+# ----------------------- INITIALIZATION -----------------------
 
 # Load .env variables into ENV
 load_dotenv()
 
 ROOT_DIR = Path(__file__).parents[2]
 input_file = os.path.join(ROOT_DIR, 'data/songs.json')
-output_file = os.path.join(ROOT_DIR, 'data/songsandlyrics.json')
+output_file = os.path.join(ROOT_DIR, 'data/songsWithLyrics.json')
 
 # Initialize RapGenius client
 genius = lyricsgenius.Genius(os.getenv("GENIUS_TOKEN"))
 
-# Get Lyric
-# song = genius.search_song("estamos bien", "bad bunny")
 
+# ----------------------- GENIUS SCRAPER THREAD CLASS -----------------------
+class geniusScraperThread(threading.Thread):
+    def __init__(self, data):
+        super(geniusScraperThread, self).__init__()
+        self.data = data
+
+    def run(self):
+        genius = lyricsgenius.Genius(os.getenv("GENIUS_TOKEN"))
+        res = []
+        for song in self.data:
+            try:
+                genius_data = genius.search_song(song["song"], song["artist"])
+
+                if genius_data and genius_data.lyrics:
+                    song["lyrics"] = genius_data.lyrics
+                    res.append(song)
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+        self.data = res
+
+
+# ----------------------- ENTRYPOINT -----------------------
 with open(input_file) as f:
     data = json.load(f)
-    for song in data:
-        genius_data = genius.search_song(song["song"], song["artist"])
 
-        if genius_data and genius_data.lyrics:
-            song["lyrics"] = genius_data.lyrics
+SONGS_PER_THREAD = 250
+thread_list = []
 
-    with open(output_file, 'w') as out:
-        json.dump(data, out)
+for i in range(0, len(data), SONGS_PER_THREAD):
+    thread = geniusScraperThread(data[i: i + SONGS_PER_THREAD])
+    thread_list.append(thread)
+    thread.start()
+
+for thread in thread_list:
+    thread.join()
+
+res = []
+for thread in thread_list:
+    res.extend(thread.data)
+
+with open(output_file, 'w') as out:
+    json.dump(res, out, indent=2)
